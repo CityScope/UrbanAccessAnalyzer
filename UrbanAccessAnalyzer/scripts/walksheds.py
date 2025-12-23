cee0cc0c7ac4a6c57c257fc7221679885ba50ecc
MiguelUrenaPliego <miguel.urena.pliego@gmail.com>
2025-12-23 16:59:17 -0500

    defragmented h3_df fix

--- UrbanAccessAnalyzer/scripts/walksheds.py
+++ UrbanAccessAnalyzer/scripts/walksheds.py
@@ -148,125 +148,134 @@ street_edges_path = os.path.normpath(output_path+f"/street_edges.gpkg")
 level_of_service_streets_path = os.path.normpath(output_path+f"/level_of_service_streets.gpkg")
 h3_results_path = os.path.normpath(output_path+f"/h3.csv")
 
-
-# Select what type of street network you want to load
-network_filter = osm.osmium_network_filter("walk+bike+primary")
-# Download the region pbf file crop it by aoi and convert to osm format
-osm.geofabrik_to_osm(
-    osm_xml_file,
-    input_file=street_path,
-    aoi=aoi_download,
-    osmium_filter_args=network_filter,
-    overwrite=False
-)
-
-if overwrite or (not os.path.isfile(street_edges_path)):
-    # Load
-    G = ox.graph_from_xml(osm_xml_file, retain_all=True, simplify=True)
-    # Project geometry coordinates to UTM system to allow euclidean meassurements in meters (sorry americans)
-    G = ox.project_graph(G,to_crs=aoi.estimate_utm_crs())
-    # street_nodes, street_edges = ox.graph_to_gdfs(G)
-    # street_nodes.to_file(street_nodes_path)
-    # street_edges.to_file(street_edges_path)
-
-    G = graph_processing.simplify_graph(G,min_edge_length=min_edge_length,min_edge_separation=min_edge_length*2,undirected=True)
-    street_nodes, street_edges = ox.graph_to_gdfs(G)
-    street_nodes.reset_index().to_file(street_nodes_path)
-    street_edges.reset_index().to_file(street_edges_path)
-else:
-    street_nodes = gpd.read_file(street_nodes_path)
-    street_nodes = street_nodes.set_index(["osmid"])
-    street_edges = gpd.read_file(street_edges_path)
-    street_edges = street_edges.set_index(["u", "v", "key"])
-    G = ox.graph_from_gdfs(street_nodes,street_edges)
-    G = ox.project_graph(G,to_crs=aoi.estimate_utm_crs())
-
-street_edges = street_edges.to_crs(aoi.crs)
-
-if (poi.geometry.type == 'Point').all():
-    poi_points_gdf = poi.copy()
-    is_point = True
-else:
-    poi_points_gdf = poi_utils.polygons_to_points(poi,street_edges)
-    is_point = False
-
-G, osmids = graph_processing.add_points_to_graph(
-    poi_points_gdf,
-    G,
-    max_dist=250+min_edge_length, # Maximum distance from point to graph edge to project the point
-    min_edge_length=min_edge_length # Minimum edge length after adding the new nodes
-)
-poi_points_gdf['osmid'] = osmids # Add the ids of the nodes in the graph to points
-poi_points_gdf = poi_points_gdf.dropna(subset=['osmid'])
-
-ls_columns = []
-level_of_service_graph = G.copy()
-for quality_str in poi_points_gdf['_service_quality'].unique():
-    poi_selection = poi_points_gdf[poi_points_gdf['_service_quality'] == quality_str]
-    if len(poi_selection) == 0:
-        continue
-
-    distance_matrix = {
-        "service_quality": [quality_str],
-    }
-    level_of_services = []
-    for d in distance_steps:
-        distance_matrix[d] = [str(d)]
-        level_of_services.append(str(d))
-
-    distance_matrix = pd.DataFrame(distance_matrix)
-    level_of_service_graph = isochrones.graph(
-        G=level_of_service_graph,
-        points=poi_points_gdf,
-        distance_matrix=distance_matrix,
-        service_quality_col = '_service_quality', # If all points have the same quality this could be None
-        level_of_services = level_of_services, # could be None and it will set to the sorted unique values of the matrix
-        min_edge_length = min_edge_length, # Do not add new nodes if there will be an edge with less than this length
-        verbose=False
+if overwrite or (not os.path.isfile(level_of_service_streets_path)):
+    # Select what type of street network you want to load
+    network_filter = osm.osmium_network_filter("walk+bike+primary")
+    # Download the region pbf file crop it by aoi and convert to osm format
+    osm.geofabrik_to_osm(
+        osm_xml_file,
+        input_file=street_path,
+        aoi=aoi_download,
+        osmium_filter_args=network_filter,
+        overwrite=False
     )
-    # Save edges as gpkg
+
+    if overwrite or (not os.path.isfile(street_edges_path)):
+        # Load
+        G = ox.graph_from_xml(osm_xml_file, retain_all=True, simplify=True)
+        # Project geometry coordinates to UTM system to allow euclidean meassurements in meters (sorry americans)
+        G = ox.project_graph(G,to_crs=aoi.estimate_utm_crs())
+        # street_nodes, street_edges = ox.graph_to_gdfs(G)
+        # street_nodes.to_file(street_nodes_path)
+        # street_edges.to_file(street_edges_path)
+
+        G = graph_processing.simplify_graph(G,min_edge_length=min_edge_length,min_edge_separation=min_edge_length*2,undirected=True)
+        street_nodes, street_edges = ox.graph_to_gdfs(G)
+        street_nodes.reset_index().to_file(street_nodes_path)
+        street_edges.reset_index().to_file(street_edges_path)
+    else:
+        street_nodes = gpd.read_file(street_nodes_path)
+        street_nodes = street_nodes.set_index(["osmid"])
+        street_edges = gpd.read_file(street_edges_path)
+        street_edges = street_edges.set_index(["u", "v", "key"])
+        G = ox.graph_from_gdfs(street_nodes,street_edges)
+        G = ox.project_graph(G,to_crs=aoi.estimate_utm_crs())
+
+    street_edges = street_edges.to_crs(aoi.crs)
+
+    if (poi.geometry.type == 'Point').all():
+        poi_points_gdf = poi.copy()
+        is_point = True
+    else:
+        poi_points_gdf = poi_utils.polygons_to_points(poi,street_edges)
+        is_point = False
+
+    G, osmids = graph_processing.add_points_to_graph(
+        poi_points_gdf,
+        G,
+        max_dist=250+min_edge_length, # Maximum distance from point to graph edge to project the point
+        min_edge_length=min_edge_length # Minimum edge length after adding the new nodes
+    )
+    poi_points_gdf['osmid'] = osmids # Add the ids of the nodes in the graph to points
+    poi_points_gdf = poi_points_gdf.dropna(subset=['osmid'])
+
+    ls_columns = []
+    level_of_service_graph = G.copy()
+    for quality_str in poi_points_gdf['_service_quality'].unique():
+        poi_selection = poi_points_gdf[poi_points_gdf['_service_quality'] == quality_str]
+        if len(poi_selection) == 0:
+            continue
+
+        distance_matrix = {
+            "service_quality": [quality_str],
+        }
+        level_of_services = []
+        for d in distance_steps:
+            distance_matrix[d] = [str(d)]
+            level_of_services.append(str(d))
+
+        distance_matrix = pd.DataFrame(distance_matrix)
+        level_of_service_graph = isochrones.graph(
+            G=level_of_service_graph,
+            points=poi_points_gdf,
+            distance_matrix=distance_matrix,
+            service_quality_col = '_service_quality', # If all points have the same quality this could be None
+            level_of_services = level_of_services, # could be None and it will set to the sorted unique values of the matrix
+            min_edge_length = min_edge_length, # Do not add new nodes if there will be an edge with less than this length
+            verbose=False
+        )
+        # Save edges as gpkg
+        level_of_service_nodes, level_of_service_edges = ox.graph_to_gdfs(level_of_service_graph)
+        level_of_service_nodes = level_of_service_nodes.rename(columns={'level_of_service':quality_str})
+        level_of_service_edges = level_of_service_edges.rename(columns={'level_of_service':quality_str})
+        if is_point == False:
+            level_of_service_nodes.loc[
+                level_of_service_nodes.intersects(poi_selection.to_crs(level_of_service_nodes.crs).union_all()),quality_str
+            ] = min(distance_steps)
+            level_of_service_edges.loc[
+                level_of_service_edges.intersects(poi_selection.to_crs(level_of_service_edges.crs).union_all()),quality_str
+            ] = min(distance_steps)
+
+        def _clean(series):
+            series = series.replace({"nan": None, "None": None})
+            # Convert np.nan/pd.NA to Python None
+            series = series.where(series.notna(), None)
+            return series.astype(object)
+
+        level_of_service_edges[quality_str] = _clean(level_of_service_edges[quality_str])
+        level_of_service_nodes[quality_str] = _clean(level_of_service_nodes[quality_str])
+
+        level_of_service_graph = ox.graph_from_gdfs(level_of_service_nodes, level_of_service_edges)
+        if quality_str in level_of_service_edges.columns:
+            ls_columns.append(quality_str)
+
     level_of_service_nodes, level_of_service_edges = ox.graph_to_gdfs(level_of_service_graph)
-    level_of_service_nodes = level_of_service_nodes.rename(columns={'level_of_service':quality_str})
-    level_of_service_edges = level_of_service_edges.rename(columns={'level_of_service':quality_str})
-    if is_point == False:
-        level_of_service_nodes.loc[
-            level_of_service_nodes.intersects(poi_selection.to_crs(level_of_service_nodes.crs).union_all()),quality_str
-        ] = min(distance_steps)
-        level_of_service_edges.loc[
-            level_of_service_edges.intersects(poi_selection.to_crs(level_of_service_edges.crs).union_all()),quality_str
-        ] = min(distance_steps)
-
-    def _clean(series):
-        series = series.replace({"nan": None, "None": None})
-        # Convert np.nan/pd.NA to Python None
-        series = series.where(series.notna(), None)
-        return series.astype(object)
-
-    level_of_service_edges[quality_str] = _clean(level_of_service_edges[quality_str])
-    level_of_service_nodes[quality_str] = _clean(level_of_service_nodes[quality_str])
-
-    level_of_service_graph = ox.graph_from_gdfs(level_of_service_nodes, level_of_service_edges)
-    if quality_str in level_of_service_edges.columns:
-        ls_columns.append(quality_str)
-
-level_of_service_nodes, level_of_service_edges = ox.graph_to_gdfs(level_of_service_graph)
-level_of_service_edges.reset_index().to_file(level_of_service_streets_path)
+    level_of_service_edges.reset_index().to_file(level_of_service_streets_path)
+else:
+    level_of_service_edges = gpd.read_file(level_of_service_streets_path)
+    level_of_service_edges = level_of_service_edges.set_index(["u", "v", "key"])
 
 if do_h3:
-    aoi['id'] = 0
-    ls_h3_df = h3_utils.from_gdf(aoi,value_column='id',value_order = [0], resolution=h3_resolution, method='first')
-    ls_h3_df = ls_h3_df[~ls_h3_df.index.duplicated(keep='first')]
-    ls_h3_df = ls_h3_df.drop(columns=ls_h3_df.columns)
+    if overwrite or (not os.path.isfile(h3_results_path)):
+        # aoi['id'] = 0
+        # ls_h3_df = h3_utils.from_gdf(aoi,value_column='id',value_order = [0], resolution=h3_resolution, method='first')
+        # ls_h3_df = ls_h3_df[~ls_h3_df.index.duplicated(keep='first')]
+        # ls_h3_df = ls_h3_df.drop(columns=ls_h3_df.columns)
+        h3_frames = []
 
-    for column in ls_columns:
-        new_ls_h3_df = h3_utils.from_gdf(
-            level_of_service_edges[[column,'geometry']],
-            resolution=h3_resolution,
-            value_column=column,
-            value_order=distance_steps,
-            buffer=10,
-            method='first'
-        )
-        ls_h3_df[column] = new_ls_h3_df[column]
+        for column in ls_columns:
+            new_ls_h3_df = h3_utils.from_gdf(
+                level_of_service_edges[[column, 'geometry']],
+                resolution=h3_resolution,
+                value_column=column,
+                value_order=distance_steps,
+                buffer=10,
+                method='first'
+            )[[column]]
 
-    ls_h3_df.reset_index().to_csv(h3_results_path)
+            h3_frames.append(new_ls_h3_df)
+
+        ls_h3_df = pd.concat(h3_frames, axis=1, join="outer")
+        ls_h3_df = ls_h3_df.copy()  # defragment
+
+        ls_h3_df.reset_index().to_csv(h3_results_path)
